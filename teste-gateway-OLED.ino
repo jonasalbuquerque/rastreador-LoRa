39ccbb099a8284b0c3ec1feb1d4fd6175d7775d0
Jonas Albuquerque <jonasalbuquerque@alu.ufc.br>
2021-02-26 13:39:31 -0300

    Push source-code of device

---
+++ teste-gateway-OLED.ino
@@ -0,0 +1,255 @@
+/*
+ * HelTec Automation(TM) LoRaWAN 1.0.2 OTAA example use OTAA, CLASS A
+ *
+ * Function summary:
+ *
+ * - use internal RTC(150KHz);
+ *
+ * - Include stop mode and deep sleep mode;
+ *
+ * - 15S data send cycle;
+ *
+ * - Informations output via serial(115200);
+ *
+ * - Only ESP32 + LoRa series boards can use this library, need a license
+ *   to make the code run(check you license here: http://www.heltec.cn/search );
+ *
+ * You can change some definition in "Commissioning.h" and "LoRaMac-definitions.h"
+ *
+ * HelTec AutoMation, Chengdu, China.
+ * 成都惠利特自动化科技有限公司
+ * https://heltec.org
+ * support@heltec.cn
+ *
+ *this project also release in GitHub:
+ *https://github.com/HelTecAutomation/ESP32_LoRaWAN
+*/
+
+#include <ESP32_LoRaWAN.h>
+#include "Arduino.h"
+#include <TinyGPS++.h>
+
+/*license for Heltec ESP32 LoRaWan, quary your ChipID relevant license: http://resource.heltec.cn/search */
+uint32_t  license[4] = {0x00000000,0x00000000,0x00000000,0x00000000};
+
+/* OTAA para*/
+uint8_t DevEui[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t AppEui[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t AppKey[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+/* ABP para*/
+uint8_t NwkSKey[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t AppSKey[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint32_t DevAddr =  ( uint32_t )0x00000000;
+
+/*LoraWan channelsmask, default channels 0-7*/ 
+uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
+
+/*LoraWan Class, Class A and Class C are supported*/
+DeviceClass_t  loraWanClass = CLASS_C;
+
+/*the application data transmission duty cycle.  value in [ms].*/
+uint32_t appTxDutyCycle = 15000;
+
+/*OTAA or ABP*/
+bool overTheAirActivation = true;
+
+/*ADR enable*/
+bool loraWanAdr = true;
+
+/* Indicates if the node is sending confirmed or unconfirmed messages */
+bool isTxConfirmed = true;
+
+/* Application port */
+uint8_t appPort = 2;
+
+/*!
+* Number of trials to transmit the frame, if the LoRaMAC layer did not
+* receive an acknowledgment. The MAC performs a datarate adaptation,
+* according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
+* to the following table:
+*
+* Transmission nb | Data Rate
+* ----------------|-----------
+* 1 (first)       | DR
+* 2               | DR
+* 3               | max(DR-1,0)
+* 4               | max(DR-1,0)
+* 5               | max(DR-2,0)
+* 6               | max(DR-2,0)
+* 7               | max(DR-3,0)
+* 8               | max(DR-3,0)
+*
+* Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
+* the datarate, in case the LoRaMAC layer did not receive an acknowledgment
+*/
+uint8_t confirmedNbTrials = 8;
+
+/*LoraWan debug level, select in arduino IDE tools.
+* None : print basic info.
+* Freq : print Tx and Rx freq, DR info.
+* Freq && DIO : print Tx and Rx freq, DR, DIO0 interrupt and DIO1 interrupt info.
+* Freq && DIO && PW: print Tx and Rx freq, DR, DIO0 interrupt, DIO1 interrupt, MCU sleep and MCU wake info.
+*/
+uint8_t debugLevel = LoRaWAN_DEBUG_LEVEL;
+
+/*LoraWan region, select in arduino IDE tools*/
+LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
+
+TinyGPSPlus gps;
+char lat_str[11], lng_str[12], gps_str[22];
+
+static void prepareTxFrame( uint8_t port )
+{
+    appDataSize = 22;//AppDataSize max value is 64
+    for (int i = 0; i < 22; i++)
+    {
+      appData[i] = gps_str[i];
+    }
+}
+
+// Add your initialization code here
+void setup()
+{
+  if(mcuStarted==0)
+  {
+    LoRaWAN.displayMcuInit();
+  }
+  Serial.begin(115200);
+  while (!Serial);
+  SPI.begin(SCK,MISO,MOSI,SS);
+  Mcu.init(SS,RST_LoRa,DIO0,DIO1,license);
+  deviceState = DEVICE_STATE_INIT;
+  /*GPS*/
+  gps_str[22] = {0};
+  Serial2.begin(9600, SERIAL_8N1,17,2);
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+  switch( deviceState )
+  {
+    case DEVICE_STATE_INIT:
+    {
+      LoRaWAN.init(loraWanClass,loraWanRegion);
+      break;
+    }
+    case DEVICE_STATE_JOIN:
+    {
+      LoRaWAN.displayJoining();
+      LoRaWAN.join();
+      break;
+    }
+    case DEVICE_STATE_SEND:
+    {
+      LoRaWAN.displaySending();
+      //GPS
+      checkGPS();
+      prepareTxFrame( appPort );
+      LoRaWAN.send(loraWanClass);
+      deviceState = DEVICE_STATE_CYCLE;
+      break;
+    }
+    case DEVICE_STATE_CYCLE:
+    {
+      // Schedule next packet transmission
+      txDutyCycleTime = appTxDutyCycle + randr( -APP_TX_DUTYCYCLE_RND, APP_TX_DUTYCYCLE_RND );
+      LoRaWAN.cycle(txDutyCycleTime);
+      deviceState = DEVICE_STATE_SLEEP;
+      break;
+    }
+    case DEVICE_STATE_SLEEP:
+    {
+      LoRaWAN.displayAck();
+      LoRaWAN.sleep(loraWanClass,debugLevel);
+      break;
+    }
+    default:
+    {
+      deviceState = DEVICE_STATE_INIT;
+      break;
+    }
+  }
+}
+
+void checkGPS()
+{
+  while (Serial2.available() > 0)
+  {
+    if (gps.encode(Serial2.read()))
+    {
+      displayGPSInfo();
+      dtostrf(gps.location.lat(), 10, 6, lat_str);
+      dtostrf(gps.location.lng(), 11, 6, lng_str);
+  
+      for (int i = 0; i < 10; i++)
+      {
+        gps_str[i] = lat_str[i];
+      }
+      gps_str[10] = ';';
+      for (int j = 0; j < 11; j++)
+      {
+        gps_str[j+11] = lng_str[j];
+      }
+      break;
+    }
+  }
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+  }
+}
+
+void displayGPSInfo()
+{
+  Serial.print(F("Satellites: "));
+  Serial.print(gps.satellites.value());
+  Serial.print(F("  Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
+}
